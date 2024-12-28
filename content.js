27649ae8ad346d0aa3340cfe6d46ad3244a3ca3f
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-12-27 23:51:30 -0500

    add: delete all LS except language preference

--- content.js
+++ content.js
@@ -1,56 +1,12 @@
-const userIdFrequency = {};
-for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    if (key.endsWith("_java")) {
-        const keySplit = key.split('_');
-        
-        const uniqueId = keySplit[1]; // Assuming the format is always leetcodeNumber_userID_java
-        if (userIdFrequency[uniqueId]) {
-            userIdFrequency[uniqueId]++;
-        } else {
-            userIdFrequency[uniqueId] = 1;
+function deleteAllStorageExceptLanguagePreset(){
+    const keyToPreserve = "1_15001158_lang"; // language preset on LC;
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key !== keyToPreserve) {
+            localStorage.removeItem(key);
+            i--;
         }
     }
-}
 
-// Find the user ID with the highest frequency
-let mostFrequentUserId = null;
-let maxFrequency = 0;
-
-for (const userId in userIdFrequency) {
-    if (userIdFrequency[userId] > maxFrequency) {
-        maxFrequency = userIdFrequency[userId];
-        mostFrequentUserId = userId;
-    }
 }
-//console.log("Most Frequent User ID:", mostFrequentUserId);
-
-async function getProblemNumber() {
-    // Make GraphQL query to LeetCode API
-    const titleSlug = window.location.pathname.split('/')[2];
-    const query = `
-        query questionTitle($titleSlug: String!) {
-            question(titleSlug: $titleSlug) {
-                questionId
-            }
-        }
-    `;
-    
-    const response = await fetch('https://leetcode.com/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: query,
-            variables: { titleSlug: titleSlug }
-        })
-    });
-    const data = await response.json();
-    return data.data.question.questionId;
-}
-async function run() {
-    const x = await getProblemNumber();
-    console.log(x);
-}
-run();
+deleteAllStorageExceptLanguagePreset();
